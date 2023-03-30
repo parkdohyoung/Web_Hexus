ddaae745e8d60f112dd9ef4e7dd25b6711f62e76
=parkdohyoung <=nhgrd@naver.com>
2023-03-30 15:56:29 +0900

    Initial commit

---
+++ src/main/kotlin/com/hexus/Web_Hexus/WebHexusApplication.kt
@@ -0,0 +1,11 @@
+package com.hexus.Web_Hexus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebHexusApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebHexusApplication>(*args)
+}
---
+++ src/test/kotlin/com/hexus/Web_Hexus/WebHexusApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hexus.Web_Hexus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebHexusApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
